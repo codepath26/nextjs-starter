bc152f1e43003907e3f49599359a42379c624eab
parth thakor <parththakor2610@gmail.com>
2023-12-02 13:01:34 +0530

    adding  the dynamic routes to aboutus

---
+++ app/pages/[productid]/page.js
@@ -0,0 +1,14 @@
+// import { useRouter } from 'next/router'
+"use client"
+import React from 'react'
+
+export default function ProductId({params}) {
+  // const router = useRouter();
+
+  console.log(params)
+  return (
+    <div>
+      page list
+    </div>
+  )
+}
